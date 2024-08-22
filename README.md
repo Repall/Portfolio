129f2a3873cd60897653ef3a28cabb5c745490eb
Repall <repall.017@gmail.com>
2024-08-23 00:03:44 +0200

    Correctif URL Image des listes de projets

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 https://repall.fr
 
 
-## Fonctionne avec : 
-- Vite, 
-- React Router,
-- Tailwind.
-
 ## Vous y retrouverez : 
 
 - mon porfolio, 
 - la liste des projets que j'ai réalisé qu'ils soient personnels ou durant ma formation. 
+
+## Ma Stack Technique
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) 
+
+## Fonctionne avec : 
+- Vite, 
+- React Router,
+- Tailwind.
--- src/components/Thumb.jsx
+++ src/components/Thumb.jsx
@@ -15,17 +15,14 @@ export default function Thumb() {
 
     return (
         <>
-            {projets.map((projet) => {
-                const imageUrl = new URL(projet.image, import.meta.url).href;
-                return (
-                    <Link to={`/projet/${projet.id}`} key={projet.id}>
-                        <div className="flex flex-col-reverse w-[335px] h-[255px] max-sm:size-64 bg-cyan-600  rounded-xl transition duration-700 hover:scale-105">
-                            <p className="z-5 absolute bg-cyan-600  text-white text-lg font-bold rounded-b-xl w-[335px] max-sm:w-64 px-5 py-2">Projet {projet.id} | {projet.title}</p>
-                            <img className="h-full w-full object-cover rounded-xl" src={imageUrl} alt="Image aperçu projet" />
-                        </div>
-                    </Link>
-                );
-            })}
+            {projets.map((projet) => (
+                <Link to={`/projet/${projet.id}`} key={projet.id}>
+                    <div className="flex flex-col-reverse w-[335px] h-[255px] max-sm:size-64 bg-cyan-600  rounded-xl transition duration-700 hover:scale-105">
+                        <p className="z-5 absolute bg-cyan-600  text-white text-lg font-bold rounded-b-xl w-[335px] max-sm:w-64 px-5 py-2">Projet {projet.id} | {projet.title}</p>
+                        <img className="h-full w-full object-cover rounded-xl" src={projet.image} alt="Image aperçu projet" />
+                    </div>
+                </Link>
+            ))}
         </>
     );
 }
