4ffa73126eca913abd603b3ce30a0e5589d71d52
Repall <repall.017@gmail.com>
2024-09-09 15:55:13 +0200

    Ajout de Vercel Analytics avec le composant React

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -29,10 +30,10 @@ const router = createBrowserRouter([
                     </ProjetsProvider>
                 ),
             },
-            // {
-            //     path: "contact",
-            //     element: <Contact />,
-            // },
+            {
+                path: "contact",
+                element: <Contact />,
+            },
             {
                 path: "*",
                 element: <P404 />,
@@ -45,6 +46,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <NextUIProvider>
             <RouterProvider router={router} />
+            <Analytics />
         </NextUIProvider>
     </React.StrictMode>
 );
